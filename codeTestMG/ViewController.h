06a222c4af56107a85f51b7935709a12b6d29d73
Marcos Garcia <1marcosgn@gmail.com>
2015-04-30 09:29:07 -0500

    Initial Commit

---
+++ codeTestMG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  codeTestMG
+//
+//  Created by Marcos Garcia on 4/30/15.
+//  Copyright (c) 2015 marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
